2fa48abb6e60d6e252ebdf2b01270ee88053c25e
courtzc <32031285+courtzc@users.noreply.github.com>
2023-07-07 14:47:35 +1000

    timestamp changes for motive starts

--- DataVisualisation/BasicSystemVisualisation.m
+++ DataVisualisation/BasicSystemVisualisation.m
@@ -1,11 +1,12 @@
 
-expID = "A1_System";
+expID = "42B";
 
 
 fileName = sprintf("Data/Session03_ManipulatedData/Time_SmoothBatonPosition/Time_Smooth_Baton_Pos_%s.mat", expID);
 tXYZ_System = load(fileName).tXYZ_System;
 figure
 hold on
+axis equal
 view(2)
 % plot3(tXYZ_System(2,:), tXYZ_System(3,:), tXYZ_System(4,:))
 plot3(tXYZ_System(:,2), tXYZ_System(:,3), tXYZ_System(:,4))
